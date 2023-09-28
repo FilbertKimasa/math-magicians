068f954ac717ec20cfd4f72639b8ac111af619a4
FilbertKimasa <filbertdan67@gmail.com>
2023-09-28 19:40:33 +0300

    Import Calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import Calculator from './components/Calculator';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="calculator-section">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
