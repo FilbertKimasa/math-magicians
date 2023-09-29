0fc21f8707f147d478d877fb8045d8d4b9b0c0ec
FilbertKimasa <filbertdan67@gmail.com>
2023-09-29 11:21:51 +0300

    Add state and import useState

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Buttons from './Buttons';
+import calculate from '../logic/Calculate';
 import './Calculator.css';
 
-const Calculator = () => (
-  <div className="main-container">
-    <input type="text" className="grid-box display" value="0" />
-    <Buttons />
-  </div>
-);
+const Calculator = () => {
+  const [display, setDisplay] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const btnClick = (btnValue) => {
+    setDisplay(calculate(display, btnValue));
+  };
+
+  return (
+    <div className="main-container">
+      <div className="display">
+        {display.total || display.operation || display.next || 0}
+      </div>
+      <Buttons btnClick={btnClick} />
+    </div>
+  );
+};
 
 export default Calculator;
