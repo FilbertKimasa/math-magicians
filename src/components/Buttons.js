b0053b1e99cad8c7ad1acf5881c0164e42c448ca
FilbertKimasa <filbertdan67@gmail.com>
2023-09-29 11:16:37 +0300

    Modify event listeners and add propps

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,72 +1,99 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Buttons() {
-  const btnClick = (btnValue) => btnValue;
+function Buttons({ btnClick }) {
   return (
     <>
-      <button type="button" className="grid-box" onClick={btnClick('AC')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('AC')}>
         AC
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('+/-')}>
+      <button
+        type="button"
+        className="grid-box"
+        onClick={() => btnClick('+/-')}
+      >
         +/-
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('%')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('%')}>
         %
       </button>
-      <button type="button" className="grid-box left" onClick={btnClick('÷')}>
+      <button
+        type="button"
+        className="grid-box left"
+        onClick={() => btnClick('÷')}
+      >
         ÷
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('7')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('7')}>
         7
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('8')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('8')}>
         8
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('9')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('9')}>
         9
       </button>
-      <button type="button" className="grid-box left" onClick={btnClick('x')}>
+      <button
+        type="button"
+        className="grid-box left"
+        onClick={() => btnClick('x')}
+      >
         x
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('4')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('4')}>
         4
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('5')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('5')}>
         5
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('6')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('6')}>
         6
       </button>
-      <button type="button" className="grid-box left" onClick={btnClick('-')}>
+      <button
+        type="button"
+        className="grid-box left"
+        onClick={() => btnClick('-')}
+      >
         -
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('1')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('1')}>
         1
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('2')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('2')}>
         2
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('3')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('3')}>
         3
       </button>
-      <button type="button" className="grid-box left" onClick={btnClick('+')}>
+      <button
+        type="button"
+        className="grid-box left"
+        onClick={() => btnClick('+')}
+      >
         +
       </button>
       <button
         type="button"
         className="grid-box btn-zero"
-        onClick={btnClick('0')}
+        onClick={() => btnClick('0')}
       >
         0
       </button>
-      <button type="button" className="grid-box" onClick={btnClick('.')}>
+      <button type="button" className="grid-box" onClick={() => btnClick('.')}>
         .
       </button>
-      <button type="button" className="grid-box left" onClick={btnClick('=')}>
+      <button
+        type="button"
+        className="grid-box left"
+        onClick={() => btnClick('=')}
+      >
         =
       </button>
     </>
   );
 }
+Buttons.propTypes = {
+  btnClick: PropTypes.func.isRequired,
+};
 
 export default Buttons;
