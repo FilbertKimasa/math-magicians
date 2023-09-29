795907141335b3276d1d2b36b04178e1915eb15c
FilbertKimasa <filbertdan67@gmail.com>
2023-09-29 08:30:51 +0300

    Add event listner to the buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,63 +1,68 @@
 import React from 'react';
 
 function Buttons() {
+  const btnClick = (btnValue) => btnValue;
   return (
     <>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('AC')}>
         AC
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('+/-')}>
         +/-
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('%')}>
         %
       </button>
-      <button type="button" className="grid-box left">
+      <button type="button" className="grid-box left" onClick={btnClick('÷')}>
         ÷
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('7')}>
         7
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('8')}>
         8
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('9')}>
         9
       </button>
-      <button type="button" className="grid-box left">
+      <button type="button" className="grid-box left" onClick={btnClick('x')}>
         x
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('4')}>
         4
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('5')}>
         5
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('6')}>
         6
       </button>
-      <button type="button" className="grid-box left">
+      <button type="button" className="grid-box left" onClick={btnClick('-')}>
         -
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('1')}>
         1
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('2')}>
         2
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('3')}>
         3
       </button>
-      <button type="button" className="grid-box left">
+      <button type="button" className="grid-box left" onClick={btnClick('+')}>
         +
       </button>
-      <button type="button" className="grid-box btn-zero">
+      <button
+        type="button"
+        className="grid-box btn-zero"
+        onClick={btnClick('0')}
+      >
         0
       </button>
-      <button type="button" className="grid-box">
+      <button type="button" className="grid-box" onClick={btnClick('.')}>
         .
       </button>
-      <button type="button" className="grid-box left">
+      <button type="button" className="grid-box left" onClick={btnClick('=')}>
         =
       </button>
     </>
