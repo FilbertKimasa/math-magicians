290a9f4f43921849f128dd074fd1c31df228226c
FilbertKimasa <filbertdan67@gmail.com>
2023-10-06 14:54:46 +0300

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './styles/App.css';
 import Quotes from './components/Quotes';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="calculator-section">
-      <Calculator />
-      <Quotes />
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="Quote" element={<Quotes />} />
+      </Routes>
+    </>
   );
 }
 
